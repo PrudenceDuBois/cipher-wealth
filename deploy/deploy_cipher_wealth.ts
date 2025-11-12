6a90f5370a015a185d7e119cdc3cb4dfa9e38a98
KirkBarnard25 <bcxyrf77997@outlook.com>
2025-11-12 11:04:00 -0800

    fix: add missing await in deployment script

--- deploy/deploy_cipher_wealth.ts
+++ deploy/deploy_cipher_wealth.ts
@@ -5,12 +5,12 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedCipherWealth = deploy("CipherWealth", {
+  const deployedCipherWealth = await deploy("CipherWealth", {
     from: deployer,
     log: true,
   });
 
-  console.log(`CipherWealth contract deployed`);
+  console.log(`CipherWealth contract: `, deployedCipherWealth.address);
 };
 export default func;
 func.id = "deploy_cipherWealth"; // id required to prevent reexecution
